3ad0615cc6bc70c8e7e5908292c6ad50245afd58
Ryan Atkinson <ratkinson@zappos.com>
2015-05-01 22:15:48 -0700

    added init for git-setup and vim-plugins scripts

---
+++ downloads/git-setup.js
@@ -0,0 +1,10 @@
+// so i can do "git push" to push my current branch
+// git config --global push.default simple
+
+/*
+  var completion = new File('git-completion.sh');
+  completion.curl('https://raw.github.com/git/git/master/contrib/completion/git-completion.bash');
+
+  var gprompt = new File('git-prompt.sh');
+  gprompt.curl('https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh');
+*/
---
+++ downloads/vim-plugins.js
@@ -0,0 +1,5 @@
+/*
+  var pathogen = new File('pathogen.vim');
+  pathogen.target = path.join(targets, 'vim/autoload/pathogen.vim');
+  pathogen.curl('https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim');
+*/
