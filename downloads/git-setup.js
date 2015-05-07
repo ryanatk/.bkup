2c641c614f4954293abf0cbed01934e426f16462
Ryan Atkinson <ratkinson@zappos.com>
2015-05-06 17:25:28 -0700

    added spotify & chromecast downloads, updated 'url' to 'link' on downloads, updated gitignore for downloads/tmp

--- downloads/git-setup.js
+++ downloads/git-setup.js
@@ -1,3 +1,4 @@
+module.exports = function () {
 // so i can do "git push" to push my current branch
 // git config --global push.default simple
 
@@ -8,3 +9,5 @@
   var gprompt = new File('git-prompt.sh');
   gprompt.curl('https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh');
 */
+  return;
+};
--- downloads/vim-plugins.js
+++ downloads/vim-plugins.js
@@ -1,5 +1,8 @@
+module.exports = function () {
 /*
   var pathogen = new File('pathogen.vim');
   pathogen.target = path.join(targets, 'vim/autoload/pathogen.vim');
   pathogen.curl('https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim');
 */
+  return;
+};
