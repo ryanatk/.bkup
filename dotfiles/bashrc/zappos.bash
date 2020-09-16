6586d86cb0267a44a5b50375943c714b6d89e7a7
Ryan Atkinson <ratkinson@zappos.com>
2020-09-16 16:12:59 -0700

    vscode updates

--- dotfiles/bashrc/zappos.bash
+++ dotfiles/bashrc/zappos.bash
@@ -2,6 +2,8 @@
 
 alias cz='cd ~/z'
 alias chz='cd ~/z/huzzah'
+alias chb='cd ~/z/huzzah-blog'
 alias czh='cd ~/z/huzzah'
 alias csa='cd ~/z/service-anything'
+alias csap='cd ~/z/service-anything-api'
 alias cbo='cd ~/z/zappos-bounties-spa'
